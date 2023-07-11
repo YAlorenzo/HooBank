1d587bba3de779a84ab6b6855dc25f8d04592831
YAlorenzo <zhenia.klimenchuk@gmail.com>
2023-07-11 20:40:37 +0200

    version_deploy

---
+++ src/components/index.js
@@ -0,0 +1,29 @@
+import Navbar from "./Navbar";
+import Hero from "./Hero";
+import Billing from "./Billing";
+import Business from "./Business";
+import CardDeal from "./CardDeal";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import FeedbackCard from "./FeedbackCard";
+import Footer from "./Footer";
+import GetStarted from "./GetStarted";
+import Stats from "./Stats";
+import Testimonials from "./Testimonials";
+import Button from "./Button";
+
+export {
+    Navbar,
+    Hero,
+    Billing,
+    Business,
+    CardDeal,
+    Clients,
+    CTA,
+    FeedbackCard,
+    Footer,
+    GetStarted,
+    Stats,
+    Testimonials,
+    Button,
+}
